5bf868435b835ebdced6e520e83707dfb26f64b7
Benjamin Lupton <b@lupton.cc>
2020-09-04 16:51:22 +1000

    __dirname compat for ESM

--- source/test.ts
+++ source/test.ts
@@ -4,12 +4,14 @@ import { rfcLogLevels } from './index.js'
 import { writeFile } from 'fs'
 import { join } from 'path'
 
+import filedirname from 'filedirname'
+const [file, dir] = filedirname()
+const jsonFilePath = join(dir, '..', 'index.json')
+
 kava.suite('rfc-log-levels', function (suite, test) {
 	test('write the json file', function (done) {
-		writeFile(
-			join(__dirname, '..', 'index.json'),
-			JSON.stringify(rfcLogLevels, null, '  '),
-			(err) => (err ? done(err) : done())
+		writeFile(jsonFilePath, JSON.stringify(rfcLogLevels, null, '  '), (err) =>
+			err ? done(err) : done()
 		)
 	})
 })
